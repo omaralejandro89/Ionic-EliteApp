8fa07ae995d3040596ec6b428bfd1215503c6022
Omar, working on hooks <omarchacin@gmail.com>
2014-10-20 19:12:23 +0200

    navigation and routing added

---
+++ www/app/teams/teamDetailController.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Omar on 10/20/14.
+ */
+(function() {
+    'use strict';
+
+    angular
+        .module('EliteApp')
+        .controller('TeamDetailController', [ '$stateParams', TeamDetailController]);
+
+
+    /* @ngInject */
+    function TeamDetailController($stateParams) {
+        /* jshint validthis: true */
+        var vm = this;
+
+        vm.activate = activate;
+        vm.title = 'TeamDetailController';
+
+        console.log('$stateParams', $stateParams);
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+        }
+
+
+    }
+
+
+})();
